444a7a28074d286c967643e771de42d12d42b91d
superboySB <604896160@qq.com>
2022-05-08 19:27:56 +0800

    combine codes of xiaotian

--- README.md
+++ README.md
@@ -5,8 +5,8 @@
 Install requirements
 
 ```shell
-conda create -n om python==3.8
-conda activate om
+conda create -n matrix python==3.8
+conda activate matrix
 conda install pytorch torchvision torchaudio cudatoolkit=11.3 tensorboard future
 pip install -r requirements.txt
 ```
@@ -14,7 +14,7 @@ pip install -r requirements.txt
 ### 2. Results in Matrix Games 
 
 Train Algorithms (config names): 
-- Our solution（???）
+- Our solution（MAPSA???）
 - VDN（vdn）
 - QMIX（qmix)  
 - WQMIX（cw_qmix，ow_qmix）
@@ -26,7 +26,8 @@ Train Algorithms (config names):
 Train:
 
 ``` sh
-python3 main.py --config=maic --env-config=one_step_matrix_game with env_args.map_name=one_step_matrix_game
+python main.py --config=maic --env-config=one_step_matrix_game with env_args.map_name=one_step_matrix_game
+python q_family.py
 ```
 #### Matrix
 
@@ -65,19 +66,19 @@ predicted_values = [[8.00, -10.62, -10.62],
 ```
 Qatten
 ```
-predicted_values = [[-22.81, -11.44, -11.44],
-                 [-11.44, 0.00, 0.00],
+predicted_values = [[-22.81, -11.45, -11.44],
+                 [-11.47, 0.00, 0.00],
                  [-11.44, 0.00, 0.00]]
 ```
 
-Qtran（refer to https://zhuanlan.zhihu.com/p/421909836）
+Qtran
 ```
 predicted_values = [[8.00, 3.99, 4.06],
                  [4.00, 0.00, 0.06],
                  [4.00, 0.00, 0.06]]
 ```
 
-Qplex（refer to https://zhuanlan.zhihu.com/p/421909836）
+Qplex
 ```
 predicted_values = [[3.94, -11.68, -11.74],
                  [-11.68, 0.21, 0.14],
@@ -91,7 +92,9 @@ predicted_values = [[-11.13, -11.13, -11.13],
                  [-11.13, 0, 0]]
 ```
 ```
-predicted_values = TODO!
+predicted_values = [[3.94, -11.68, -11.75],
+                 [-11.68, 0.21, 0.14],
+                 [-11.75, 0.13, 3.97]]
 ```
 
 
--- q_family.py
+++ q_family.py
@@ -2,9 +2,9 @@
 # @Time    : 2021/7/19 16:13
 # @Author  : HaoXiaotian
 # @Contact : xiaotianhao@tju.edu.cn
-# @File    : QPLEX.py
+# @File    : q_family.py
 
-import torch as th
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
@@ -15,21 +15,39 @@ import torch.nn.init as init
 import math
 
 
-class QPLEX(nn.Module):
-    def __init__(self, agent_num, action_num, hidden_num):
-        super(QPLEX, self).__init__()
+class QFamily(nn.Module):
+    def __init__(self, algo, agent_num, action_num, hidden_num):
+        super(QFamily, self).__init__()
+        self.algo = algo
+
         self.agent_num = agent_num
         self.action_num = action_num
+        self.hidden_num = hidden_num
 
         # (agent_num, action_num)
-        self.individual_qs = Parameter(th.Tensor(1, agent_num, action_num))
+        self.individual_qs = Parameter(torch.randn(1, agent_num, action_num))
 
-        # lambda net
-        self.lambda_net = nn.Sequential(
-            nn.Linear(self.agent_num * self.action_num, hidden_num),
-            nn.ReLU(),
-            nn.Linear(hidden_num, self.agent_num),
-        )
+        if algo in ["vdn", "vdn_qtran"]:
+            pass
+        elif algo in "weighted_vdn":
+            self.w1 = Parameter(torch.randn([agent_num, 1], dtype=torch.float32))
+            self.b1 = Parameter(torch.randn([1, 1], dtype=torch.float32))
+        elif algo in ["qmix", "qmix_qtran"]:
+            # (agent_num, embedding_dim)
+            self.w1 = Parameter(torch.randn([agent_num, hidden_num], dtype=torch.float32))
+            self.b1 = Parameter(torch.randn([1, hidden_num], dtype=torch.float32))
+            # (embedding_dim, 1)
+            self.w2 = Parameter(torch.randn([hidden_num, 1], dtype=torch.float32))
+            self.b2 = Parameter(torch.randn([1, 1], dtype=torch.float32))
+        elif algo == "qplex":
+            # lambda net
+            self.lambda_net = nn.Sequential(
+                nn.Linear(agent_num * action_num, hidden_num),
+                nn.ReLU(),
+                nn.Linear(hidden_num, agent_num),
+            )
+        else:
+            raise NotImplementedError
 
         # init parameters
         self._init_parameters()
@@ -57,21 +75,34 @@ class QPLEX(nn.Module):
         assert batch_action.shape[0] == batch_size
         assert batch_action.shape[1] == self.agent_num
         assert batch_action.shape[2] == 1
-        q_upper, _ = self.individual_qs.max(dim=2, keepdim=False)
+        # Get action values (batch_size, agent_num)
+        selected_individual_qs = torch.gather(self.individual_qs.expand(batch_size, self.agent_num, self.action_num),
+                                              dim=2, index=batch_action.long()).squeeze(2)  # Remove the last dim
 
-        # (batch_size, agent_num)
-        selected_individual_qs = th.gather(self.individual_qs.expand(batch_size, self.agent_num, self.action_num),
-                                           dim=2, index=batch_action.long()).squeeze(2)  # Remove the last dim
-        adv = selected_individual_qs - q_upper
+        # Calculate q_total
+        if self.algo in ["vdn", "vdn_qtran"]:
+            q_tot = selected_individual_qs.sum(dim=1, keepdim=True)
+        elif self.algo == "weighted_vdn":
+            q_tot = torch.mm(selected_individual_qs, torch.abs(self.w1)) + self.b1
+        elif self.algo in ["qmix", "qmix_qtran"]:
+            hidden = F.elu(torch.mm(selected_individual_qs, torch.abs(self.w1)) + self.b1)
+            q_tot = torch.mm(hidden, torch.abs(self.w2)) + self.b2
+        elif self.algo == "qplex":
+            q_upper, _ = self.individual_qs.max(dim=2, keepdim=False)
+            # (batch_size, agent_num)
+            adv = selected_individual_qs - q_upper
 
-        onehot_batch_action = th.zeros((*batch_action.shape[:-1], self.action_num), dtype=th.float32,
-                                       device=batch_action.device). \
-            scatter_(-1, batch_action, 1.0).view(batch_size, -1)  # [batch, agent_num * action_num]
-        w_lambda = th.abs(self.lambda_net(onehot_batch_action))
+            onehot_batch_action = torch.zeros((*batch_action.shape[:-1], self.action_num), dtype=torch.float32,
+                                              device=batch_action.device). \
+                scatter_(-1, batch_action, 1.0).view(batch_size, -1)  # [batch, agent_num * action_num]
+            w_lambda = torch.abs(self.lambda_net(onehot_batch_action))  # 约束lambda>0
 
-        v_tot = q_upper.sum(dim=1, keepdim=True)  # current maximum point
-        adv_tot = (adv * w_lambda).sum(dim=1, keepdim=True)  # weighted sum
-        q_tot = v_tot + adv_tot
+            v_tot = q_upper.sum(dim=1, keepdim=True)  # current maximum point
+            adv_tot = (adv * w_lambda).sum(dim=1, keepdim=True)  # weighted sum
+            q_tot = v_tot + adv_tot
+        else:
+            raise NotImplementedError
+
 
         if print_log:
             print("******************* [q_i] Learned individual q tables *******************")
@@ -84,8 +115,17 @@ class QPLEX(nn.Module):
                 print(individual_q.tolist())
                 print("--------------------------------------\n")
 
-        # (1) VDN loss
-        loss = th.mean((q_tot - q_joint) ** 2)
+        # Calculate loss
+        if self.algo in ["vdn_qtran", "qmix_qtran"]:
+            # greedy actions
+            individual_greedy_action = self.individual_qs.max(dim=2, keepdim=True)[1]
+            # the sample in current batch in which the actions == greedy actions
+            max_point_mask = ((individual_greedy_action == batch_action).long().sum(axis=1) == self.agent_num).float()
+            q_clip = torch.max(q_tot, q_joint).detach()  # Qtran核心： to ensure q_tot >= q_label
+            loss = torch.mean(
+                max_point_mask * ((q_tot - q_joint) ** 2) + (1 - max_point_mask) * ((q_tot - q_clip) ** 2))
+        else:
+            loss = torch.mean((q_tot - q_joint) ** 2)
         return q_tot, loss
 
 
@@ -101,22 +141,22 @@ def train(case=0):
     if case == 0:
         batch_size = 4
         action_num = 2
-        batch_action = th.from_numpy(
+        batch_action = torch.from_numpy(
             np.array([
                 [[0], [0]],
                 [[0], [1]],
                 [[1], [0]],
                 [[1], [1]]
-            ], dtype=np.long)
+            ], dtype=np.compat.long)
         )
-        q_joint = th.from_numpy(np.reshape(
+        q_joint = torch.from_numpy(np.reshape(
             np.array([1, 0, 0, 1], dtype=np.float32)
             , newshape=[batch_size, 1])
         )
     elif case == 1:
         batch_size = 9
         action_num = 3
-        batch_action = th.from_numpy(
+        batch_action = torch.from_numpy(
             np.array([
                 [[0], [0]],
                 [[0], [1]],
@@ -127,36 +167,17 @@ def train(case=0):
                 [[2], [0]],
                 [[2], [1]],
                 [[2], [2]],
-            ], dtype=np.long)
+            ], dtype=np.compat.long)
         )
-        q_joint = th.from_numpy(np.reshape(
-            np.array([8, -12, -12, -12, 0, 0, -12, 0, 0], dtype=np.float32)
-            , newshape=[batch_size, 1])
-        )
-    elif case == 2:
-        batch_size = 9
-        action_num = 3
-        batch_action = th.from_numpy(
-            np.array([
-                [[0], [0]],
-                [[0], [1]],
-                [[0], [2]],
-                [[1], [0]],
-                [[1], [1]],
-                [[1], [2]],
-                [[2], [0]],
-                [[2], [1]],
-                [[2], [2]],
-            ], dtype=np.long)
-        )
-        q_joint = th.from_numpy(np.reshape(
+        q_joint = torch.from_numpy(np.reshape(
             np.array([8, 3, 2, -12, -13, -14, -12, -13, -14], dtype=np.float32)
             , newshape=[batch_size, 1])
         )
-    elif case == 3:
+
+    elif case == 2:
         batch_size = 9
         action_num = 3
-        batch_action = th.from_numpy(
+        batch_action = torch.from_numpy(
             np.array([
                 [[0], [0]],
                 [[0], [1]],
@@ -167,16 +188,37 @@ def train(case=0):
                 [[2], [0]],
                 [[2], [1]],
                 [[2], [2]],
-            ], dtype=np.long)
+            ], dtype=np.compat.long)
         )
-        q_joint = th.from_numpy(np.reshape(
+        q_joint = torch.from_numpy(np.reshape(
+            np.array([8, -12, -12, -12, 0, 0, -12, 0, 0], dtype=np.float32)
+            , newshape=[batch_size, 1])
+        )
+
+    elif case == 3:
+        batch_size = 9
+        action_num = 3
+        batch_action = torch.from_numpy(
+            np.array([
+                [[0], [0]],
+                [[0], [1]],
+                [[0], [2]],
+                [[1], [0]],
+                [[1], [1]],
+                [[1], [2]],
+                [[2], [0]],
+                [[2], [1]],
+                [[2], [2]],
+            ], dtype=np.compat.long)
+        )
+        q_joint = torch.from_numpy(np.reshape(
             np.array([8, -12, -12, -12, 6, 0, -12, 0, 6], dtype=np.float32)
             , newshape=[batch_size, 1])
         )
     elif case == 4:
         batch_size = 9
         action_num = 3
-        batch_action = th.from_numpy(
+        batch_action = torch.from_numpy(
             np.array([
                 [[0], [0]],
                 [[0], [1]],
@@ -187,38 +229,34 @@ def train(case=0):
                 [[2], [0]],
                 [[2], [1]],
                 [[2], [2]],
-            ], dtype=np.long)
+            ], dtype=np.compat.long)
         )
-        q_joint = th.from_numpy(np.reshape(
+        q_joint = torch.from_numpy(np.reshape(
             np.array([8, -12, -12, -12, 0, 0, -12, 0, 0], dtype=np.float32) + 12
             , newshape=[batch_size, 1])
         )
     else:
         raise NotImplementedError
 
-    qplex = QPLEX(agent_num=agent_num, action_num=action_num, hidden_num=hidden_num)
-
-    # optimizer = th.optim.RMSprop(params=qplex.parameters(), lr=0.01, alpha=0.99, eps=0.00001)
-    # optimizer = th.optim.SGD(params=qplex.parameters(), lr=0.01)
-    optimizer = th.optim.Adam(params=qplex.parameters(), lr=0.01)
+    one_step_q_network = QFamily(algo=algo, agent_num=agent_num, action_num=action_num, hidden_num=hidden_num)
+    optimizer = torch.optim.Adam(params=one_step_q_network.parameters(), lr=0.01)
 
     for epoch in range(round):
         # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Part I: non-monotonic QMIX %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        q_tot, loss = qplex.forward(batch_size, batch_action, q_joint)
+        q_tot, loss = one_step_q_network.forward(batch_size, batch_action, q_joint)
         if epoch % 100 == 0:
             print("Iter={}: MSE loss={}".format(epoch, loss.item()))
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
-    q_tot, _ = qplex.forward(batch_size, batch_action, q_joint, print_log=True)
+    q_tot, _ = one_step_q_network.forward(batch_size, batch_action, q_joint, print_log=True)
 
     print("******************* Predicted Q_tot: *******************")
     q_print = q_tot.detach().tolist()
     for row in range(action_num):
         start_pos = row * action_num
         print(q_print[start_pos: start_pos + action_num])
-    print()
 
     print("******************* True Q_joint: *******************")
     q_print = q_joint.detach().tolist()
@@ -228,24 +266,14 @@ def train(case=0):
 
 
 if __name__ == "__main__":
-    # monotonic
-    seed = 12345  # top left
-    # seed = 12  # down right
-    # seed = 123456  # all 0.5
-    round = 4000
+    # algo = "vdn"
+    # algo = "weighted_vdn"
+    # algo = "qmix"
+    # algo = "vdn_qtran"
+    # algo = "qmix_qtran"
+    algo = "qplex"
 
-    # non-monotonic
-    # seed = 1  # true decomposition
-    # seed = 2  # all 0.5
-    # random.seed(seed)
-    # np.random.seed(seed)
-    # th.manual_seed(seed)
-    # random_sample = True
-    random_sample = False
+    round = 4000
     hidden_num = 2
 
-    # non monotonic
-    train(case=1)
-
-    # monotonic
-    # train(case=2)
+    train(case=3)
